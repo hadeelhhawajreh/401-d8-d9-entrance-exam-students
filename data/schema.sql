cee213cda95042b142932636b65c01e1b7e832cd
Hadeel H Al-Hawajreh <hadeelhhawajreh@gmail.com>
2020-11-18 12:30:16 +0200

    solve lab

---
+++ data/schema.sql
@@ -0,0 +1,7 @@
+create table potter(
+    id serial not null primary key,
+    image varchar(255),
+    name varchar(255),
+    patronus varchar(255),
+    alive varchar(255)
+);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 // -------------------------
 // Application Dependencies
 // -------------------------
@@ -11,7 +11,7 @@ const methodOverride = require('method-override');
 // Environment variables
 // -------------------------
 require('dotenv').config();
-const HP_API_URL = process.env.HP_API_URL;
+// const HP_API_URL = process.env.HP_API_URL;
 
 // -------------------------
 // Application Setup
@@ -40,24 +40,103 @@ client.on('error', err => console.error(err));
 app.set('view engine', 'ejs');
 
 
+
+//////
+function Harry(obj){
+  this.image=obj.image;
+  this.name=obj.name;
+  this.patronus=obj.patronus;
+  this.alive=obj.alive;
+}
 // ----------------------
 // ------- Routes -------
 // ----------------------
-
+app.get('/home', homefun);
+app.get('/house/character',viewChar);
+app.post('/my-character',addingToDB);
+app.get('/my-character',readingFromDB);
+app.get('/my-character/:id',showOne);
+app.put('/my-character/:id',updating);
+app.delete('/my-character/:id',deleting);
 
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
+function homefun(req,res){
+  res.render('home',{msg:'inside home'});
+}
+function viewChar(req, res) {
+  let houseName=req.query.house;
+  //http://hp-api.herokuapp.com/api/characters/house/gryffindor
+  superagent.get(`http://hp-api.herokuapp.com/api/characters/house/${houseName}`).then(data => {
+    let arr=data.body.map(ele=>{
+      return new Harry(ele);
+    });
+    res.render('house',{house:arr});
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+function addingToDB(req,res){
+  let sql='INSERT INTO potter (image,name, patronus, alive)VALUES ($1,$2,$3,$4);';
+  let val=[req.body.image,req.body.name,req.body.patronus,req.body.alive];
+  client.query(sql,val).then(data=>{
+    res.redirect('/my-character');
+  }).catch(err => {
+    console.log(err);
+  });
+}
 
 
 
-// -----------------------------------
-// --- CRUD Pages Routes functions ---
-// -----------------------------------
+function readingFromDB(req,res){
+  let sql='select image, name ,patronus,alive from potter;';
+  client.query(sql).then(data=>{
+      console.log('inside reading from db');
+      console.log(data.rows);
+    res.render('favorite',{dataAll:data.rows});
+  }).catch(err => {
+    console.log(err);
+  });
+}
+function showOne(req,res){
+  let sql='select image, name ,patronus,alive from potter where id=$1;';
+  let val=[req.params.id];
+  client.query(sql,val).then(data=>{
+    res.render('details',{oneItem:data.rows[0]});
+
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+function updating(req,res){
+  let sql='UPDATE potter SET name = $1,patronus = $2,alive = $3 WHERE id=$4;';
+  let val=[req.body.name,req.body.patronus,req.body.alive,req.params.id];
+  client.query(sql,val).then(data=>{
+    res.redirect('/my-character');
+  }).catch(err => {
+    console.log(err);
+  });
+}
+
+
+function deleting(req,res){
+
+  let sql=' DELETE FROM potter WHERE id=$4;';
+  let val=[req.params.id];
+  client.query(sql,val).then(data=>{
+    res.redirect('/my-character');
+  }).catch(err => {
+    console.log(err);
+  });
+}
 
 
 
 // Express Runtime
 client.connect().then(() => {
-    app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 }).catch(error => console.log(`Could not connect to database\n${error}`));
+
